e263172c68137b67279012938e63fa6d20aeeb32
Vedansh <superuser.ntsystems@outlook.com>
2025-07-13 10:45:19 +0530

    perf: remove unused files and seo markups.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,8 @@ export default defineConfig({
     resolve: {
       alias: {
         '@': '/src',
-        '@components': '/src/components'
+        '@components': '/src/components',
+        '@lib': '/src/lib'
       }
     }
   },
